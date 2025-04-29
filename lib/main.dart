2ea9df83e005c599932718cbbb2b12099b015521
zeriong <jaeryong95@gmail.com>
2025-04-29 21:01:15 +0900

    recap - about dart class & type

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
+class Player {
+  String? name;
+  Player({required this.name});
+}
+
 void main() {
+  var zeriong = Player(name: "zeriong");
   runApp(App());
 }
 
