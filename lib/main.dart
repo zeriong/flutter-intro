d9dd06be6791beeee204a4b0ed2b49505f9ba1d8
zeriong <jaeryong95@gmail.com>
2025-05-04 18:46:19 +0900

    Column위젯의 정렬 세팅 및 background color에 custom color를 활용

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,49 @@
 import 'package:flutter/material.dart';
 
-class Player {
-  String? name;
-  Player({required this.name});
-}
-
 void main() {
-  var zeriong = Player(name: "zeriong");
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
+        // custom color 지정할 때 0xFF + [ 컬러 코드 ] ( 아래는 코드: #181818 )
+        backgroundColor: const Color(0xFF181818),
+
+        // body
         body: Column(
           children: [
-            SizedBox(
+            // 박스를 추가하여 간격을 만듦
+            const SizedBox(
               height: 80,
             ),
+
+            // 헤더 Row
             Row(
+              // Row의 Align을 end로 설정 ( justify-content: end 와 유사 )
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Column(
+                  // 해당 컬럼 item 내부 정렬을 end로 설정 ( justify-content: end 와 유사 )
+                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    Text(
+                    const Text(
                       "Hey, Selena",
                       style: TextStyle(
                         color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800,
                       ),
                     ),
                     Text(
                       "Welcome back",
                       style: TextStyle(
-                        color: Colors.white,
+                        color: Colors.white.withOpacity(0.7),
+                        fontSize: 18,
                       ),
                     ),
                   ],
