89e707b926eff519bae888a09f2d924151310e05
zeriong <jaeryong95@gmail.com>
2025-05-04 15:33:19 +0900

    backgroundColor 지정 방식 학습

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello Flutter"),
-        ),
+        backgroundColor: Colors.black,
         body: Center(
           child: Text("Hello World"),
         ),
