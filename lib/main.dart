9998c041298f03721e344ff667c73331a2e9efdb
zeriong <jaeryong95@gmail.com>
2025-05-12 20:47:24 +0900

    카드 폼 추가

--- lib/main.dart
+++ lib/main.dart
@@ -117,10 +117,12 @@ class App extends StatelessWidget {
                 ],
               ),
 
+              // 박스를 추가하여 간격을 만듦
               const SizedBox(
                 height: 100,
               ),
 
+              // Wallets / View All Row
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -138,6 +140,58 @@ class App extends StatelessWidget {
                         color: Colors.white.withOpacity(0.8), fontSize: 18),
                   ),
                 ],
+              ),
+
+              // 박스를 추가하여 간격을 만듦
+              const SizedBox(
+                height: 20,
+              ),
+
+              Container(
+                decoration: BoxDecoration(color: Color(0xFF1F2123)),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Euro",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "6 428",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                "EUR",
+                                style: TextStyle(
+                                  color: Colors.white.withOpacity(0.7),
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
               )
             ],
           ),
