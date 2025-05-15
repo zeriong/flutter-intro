25ddf3202d23f0a95cdcdafe6fd9c62bc552f2d6
zeriong <jaeryong95@gmail.com>
2025-05-15 19:59:19 +0900

    공용으로 활용 될 currencyCard 위젯추가

---
+++ lib/widgets/currencyCard.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class CurrencyCard extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String amount;
+  final String currency;
+
+  final double? iconSize;
+  final double? offsetX;
+  final double? offsetY;
+
+  final Color? bgColor;
+  final Color? titleColor;
+  final Color? amountColor;
+  final Color? currencyColor;
+  final Color? iconColor;
+
+  const CurrencyCard({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.amount,
+    required this.currency,
+    this.bgColor = const Color(0xFF1F2123),
+    this.titleColor,
+    this.amountColor,
+    this.currencyColor,
+    this.iconColor,
+    this.iconSize,
+    this.offsetX,
+    this.offsetY,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    color: titleColor ?? Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      amount,
+                      style: TextStyle(
+                        color: amountColor ?? Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      currency,
+                      style: TextStyle(
+                        color: currencyColor ?? Colors.white.withOpacity(0.7),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+            Transform.scale(
+                scale: 2.2,
+                child: Transform.translate(
+                  offset: Offset(offsetX ?? -5, offsetY ?? 12),
+                  child: Icon(
+                    icon,
+                    size: iconSize ?? 88,
+                    color: iconColor ?? Colors.white,
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
