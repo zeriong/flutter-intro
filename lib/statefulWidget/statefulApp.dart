9854ec4824b0d43f7603edf6e1e912db5a15f4ca
zeriong <jaeryong95@gmail.com>
2025-05-23 13:34:43 +0900

    배열 + for문을 활용하여 increase number가 추가적으로 쌓이며 렌더링 되는 방식 학습

--- lib/statefulWidget/statefulApp.dart
+++ lib/statefulWidget/statefulApp.dart
@@ -8,14 +8,23 @@ class StatefulApp extends StatefulWidget {
 }
 
 class _StatefulAppState extends State<StatefulApp> {
-  int count = 0;
+  // ? 단순 카운터에 대한 학습
+  // int count = 0;
+  //
+  // void onClicked() {
+  //   count += 1;
+  //   // setState(() {
+  //   //   count += 1;
+  //   // });
+  //   setState(() {});
+  // }
+
+  List<int> numbers = [];
 
   void onClicked() {
-    count += 1;
-    // setState(() {
-    //   count += 1;
-    // });
-    setState(() {});
+    setState(() {
+      numbers.add(numbers.length);
+    });
   }
 
   @override
@@ -33,11 +42,14 @@ class _StatefulAppState extends State<StatefulApp> {
                 style: TextStyle(fontSize: 20),
               ),
 
-              // 카운트를 보여 줄 요소
-              Text(
-                "$count",
-                style: const TextStyle(fontSize: 20),
-              ),
+              // // 카운트를 보여 줄 요소
+              // Text(
+              //   "$count",
+              //   style: const TextStyle(fontSize: 20),
+              // ),
+
+              // 카운트 시 배열 내부에 추가하여 Column의 자식으로서 하나씩 추가렌더링이 됨
+              for (var n in numbers) Text('$n'),
 
               // 아이콘 버튼
               IconButton(
