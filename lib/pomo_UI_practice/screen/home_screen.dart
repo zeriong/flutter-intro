dcd616ed20164883be1274760ae1570437e08ac6
zeriong <jaeryong95@gmail.com>
2025-06-03 16:35:52 +0900

    Flexible 첫번째 박스 theme을 활용 및 alignment를 활용하여 글자 제대로 보이도록 정렬

--- lib/pomo_UI_practice/screen/home_screen.dart
+++ lib/pomo_UI_practice/screen/home_screen.dart
@@ -11,14 +11,26 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor, // 테마 지정
       body: Column(
         children: [
           Flexible(
             // flex 비율을 결정
             flex: 1,
 
-            child: Container(),
+            child: Container(
+              // 최상단에 붙는 문제 해결
+              alignment: Alignment.bottomCenter,
+
+              child: Text(
+                "25:00",
+                style: TextStyle(
+                  color: Theme.of(context).cardColor, // 테마 지정
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
           ),
           Flexible(
             // flex 비율을 결정 ( 2이기 때문에 위아래 Flexible 박스보다 2배 큼 )
